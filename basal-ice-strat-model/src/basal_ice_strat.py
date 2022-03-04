10d20c8dea1be2f0ee284e4e9727808aa5f02781
Ethan Pierce <ethan.pierce@colorado.edu>
2022-03-04 12:36:31 -0700

    Implement IceColumn data structure

--- basal-ice-strat-model/src/basal_ice_strat.py
+++ basal-ice-strat-model/src/basal_ice_strat.py
@@ -9,6 +9,51 @@ Longer description goes here.
 
 import firedrake
 
+class Stratigrapher:
+    '''Track the basal ice stratigraphy beneath an ice mass.
+
+    Intended to operate on one or more IceColumn objects, with functions to handle
+    melt, freeze-on, regelation, and frozen fringe development.
+
+    Attributes:
+        None
+
+    '''
+
+    def __init__(self):
+        '''Initialize the Stratigrapher with fields from an ice dynamics model.'''
+        pass
+
+    def erode(self):
+        '''Calculate subglacial erosion beneath the ice mass.'''
+        pass
+
+    def melt(self):
+        '''Melt the lowest layer(s) of the IceColumn.'''
+        pass
+
+    def freeze(self):
+        '''Freeze on a new layer at the base of the IceColumn.'''
+        pass
+
+    def regeler(self):
+        '''Entrain a new layer of sediment via regelation.'''
+        pass
+
+    def evolve_fringe(self):
+        '''Update the state of the frozen fringe beneath the ice mass.'''
+        pass
+
+    def update_thermal_state(self):
+        '''Update the thermal state at the base of the ice mass.'''
+        pass
+
+    def update(self, dt):
+        '''Simulate the evolution of the IceColumn over one time step.'''
+        pass
+
+
+
 class IceColumn:
     '''Data structure for one column of ice.
 
@@ -20,19 +65,23 @@ class IceColumn:
 
     '''
 
-    def __init__(self, x: float, y: float, flow_field):
-        '''Initialize the IceColumn with a position and a velocity field.'''
-        
-        self.loc = {'x': x, 'y': y}
-        self.flow_field = flow_field
-        self.div_field = firedrake.div(self.flow_field)
+    def __init__(self, x: float, y: float, sliding_velocity, ice_thickness):
+        '''Initialize the IceColumn with a position, velocity field, and thickness field.'''
 
-        # Velocity and divergence at the current location
-        self.velocity = None
+        self.loc = {'x': x, 'y': y}
+        self.sliding_velocity_field = sliding_velocity
+        self.divergence_field = firedrake.div(self.sliding_velocity_field)
+        self.ice_thickness = ice_thickness
+
+        # Velocity, divergence, and ice thickness at the current location
+        self.sliding_velocity = None
         self.divergence = None
+        self.total_height = None
+        self.update_field_vars()
 
         # Simulation variables
         self.height = 0.0
+        self.dH_ratio = 1.0
         self.layers = []
         self.time_elapsed = 0.0
 
@@ -40,31 +89,60 @@ class IceColumn:
         '''Add an IceLayer to the IceColumn.'''
 
         self.layers.append(new_layer)
-        pass
+        self.height += new_layer.height
 
-    def rm_layer(self):
+    def rm_layer(self, idx: int):
         '''Remove a layer from the IceColumn.'''
-        pass
 
-    def mutate_layer(self):
+        self.height -= self.layers[idx].height
+        del self.layers[idx]
+
+    def update_layer(self, idx, **kwargs):
         '''Change the attributes of a layer.'''
-        pass
 
-    def interpolate(self):
-        '''Interpolate the column position within the velocity field.'''
-        pass
+        for key, val in kwargs:
+            setattr(self.layers[idx], key, val)
 
-    def move(self):
-        '''Change the position of the IceColumn.'''
-        pass
+    def update_field_vars(self):
+        '''Interpolate to find the velocity, divergence, and total height at the current location.'''
 
-    def alter_height(self):
+        self.sliding_velocity = self.sliding_velocity_field.at(self.loc['x'], self.loc['y'])
+        self.divergence = self.divergence_field.at(self.loc['x'], self.loc['y'])
+        self.total_height = self.ice_thickness.at(self.loc['x'], self.loc['y'])loc['x'], self.loc['y']])
+
+    def update_loc(self, dt):
+        '''Change the position of the IceColumn, given the time step dt.'''
+
+        self.update_velocity()
+        ux, uy = self.sliding_velocity
+        self.loc['x'] += ux * dt
+        self.loc['y'] += uy * dt
+
+    def calc_deformation(self, dt):
+        '''Update the expected change in thickness from advective deformation.'''
+        dhdt = -self.divergence
+        total_dh = dhdt * dt
+        self.dh_ratio = self.total_height / (self.total_height + total_dh)
+
+    def update_height(self):
         '''Change the height of the IceColumn and all associated layers.'''
-        pass
+
+        ratio = self.dh_ratio
+        self.height *= ratio
+
+        for idx in range(len(self.layers)):
+            new_h = self.layers[idx].height * ratio
+            self.update_layer(idx, height = new_h)
 
     def update(self, dt):
         '''Run one time step, advect the column, and update layer heights.'''
-        pass
+
+        self.update_field_vars()
+        self.update_loc(dt)
+        self.calc_deformation(dt)
+        self.update_height(dt)
+        self.time_elapsed += dt
+
 
 
 class IceLayer:
