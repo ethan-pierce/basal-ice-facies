db828a8e5232e5a82bc2f3fafaaeef3c4f0e7bfe
Ethan Pierce <ethan.pierce@colorado.edu>
2022-06-15 11:52:25 -0600

    Set up framework for config file

--- basis/src/flowband.py
+++ basis/src/flowband.py
@@ -1,38 +1,16 @@
 '''Class to generate a flowband and calculate boundary conditions at the ice-sediment interface.'''
 
+import numpy as np
 from dataclasses import dataclass
 from landlab import RasterModelGrid
 import rasterio as rio
 
-@dataclass
-class Flowband:
-    '''Dataclass that wraps a 2D RasterModelGrid in the x-z plane.'''
-    grid: RasterModelGrid
-
 class FlowbandGenerator:
-    '''Class to generate a Lagrangian flowband and populate grid fields.'''
+    '''Class to generate a Lagrangian flowband and populate grid fields at nodes.'''
 
-    def __init__(self, files: list):
-        '''Initialize the utility with a list of input files.'''
+    def __init__(self, config: str):
+        '''Initialize the utility with a configuration file.'''
 
         self.variables = []
-
-        # Read each file and store the first band of raster data in this class.
-        for file in files:
-            file_name = file.split('/')[-1]
-            var_name = file_name.split('.')[0]
-            self.variables.append(var_name)
-
-            with rio.open(file) as f:
-                data = f.read(1)
-
-                setattr(self, var_name, data)
-
-        # Set up the 2D x-y mesh
-        self.shape = getattr(self, self.variables[0]).shape
-        self.grid = RasterModelGrid(self.shape)
-
-        # Assert that all variable fields have the same shape as the first variable listed
-        for var in self.variables:
-            if getattr(self, var).shape != self.shape:
-                raise ValueError(var + ' has shape ' + str(getattr(self, var).shape) + ', but should be ' + str(self.shape))
+        self.crs = None
+        self.grid = None
--- basis/test/test_flowband_generator.py
+++ basis/test/test_flowband_generator.py
@@ -1,46 +1,20 @@
 import pytest
 
-from src.flowband import Flowband, FlowbandGenerator
+from src.flowband import FlowbandGenerator
 
 def test_always_passes():
     '''This test always passes.'''
     assert True
 
-@pytest.fixture
-def flowband():
-    return Flowband(None)
-
-class TestFlowband:
-    '''Test the Flowband dataclass.'''
-
-    def test_initialization(self, flowband):
-        '''Test initialization on an artificial grid.'''
-        assert flowband.grid == None
-
-
-test_vars = ['ice_thickness', 'velocity_x', 'velocity_y']
-test_files = ['./test/test_data/' + i + '.tif' for i in test_vars]
+test_config = ''
 
 @pytest.fixture
 def generator():
-    return FlowbandGenerator(test_files)
+    return FlowbandGenerator(test_config)
 
 class TestFlowbandGenerator:
     '''Test the FlowbandGenerator utility.'''
 
-    def test_initialization(self, generator):
+    def test_input_file_io(self, generator):
         '''Test initialization with given input files.'''
-
-        for var in test_vars:
-            '''Test that variable list is set up.'''
-            assert var in generator.variables
-
-        for var in test_vars:
-            '''Test that fields exist and are not None.'''
-            assert type(getattr(generator, var)) is not None
-
-        for var in test_vars:
-            '''Test that input fields have the same shape as the first variable listed.'''
-            assert getattr(generator, var).shape == getattr(generator, test_vars[0]).shape
-
-    
+        assert generator
