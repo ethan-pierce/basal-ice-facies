524fa740b14db8ebfaf35bc841f79042b96600d3
Ethan Pierce <ethan.pierce@colorado.edu>
2022-08-09 22:51:18 +0000

    Write frozen fringe model

--- basis/src/basal_ice_stratigrapher.py
+++ basis/src/basal_ice_stratigrapher.py
@@ -84,27 +84,162 @@ class BasalIceStratigrapher:
         if 'subglacial_melt__rate' not in self.grid.at_node.keys():
             self.grid.add_zeros('subglacial_melt__rate', at = 'node')
 
+        if 'frictional_heat__flux' not in self.grid.at_node.keys():
+            self.grid.add_zeros('frictional_heat__flux', at = 'node')
+
         rho = self.parameters['ice_density']
         L = self.parameters['ice_latent_heat']
         ub = self.grid.at_node['glacier__sliding_velocity'][:]
         N = self.grid.at_node['glacier__effective_pressure'][:]
         mu = self.parameters['friction_coefficient']
+
         frictional = ub * mu * N
+        self.grid.at_node['frictional_heat__flux'][:] = frictional
+
         geothermal = self.parameters['geothermal_heat_flux']
 
         self.grid.at_node['subglacial_melt__rate'][:] = (frictional + geothermal) / (rho * L)
 
-    def calc_fringe_growth_rate(self):
-        """Calculates the growth rate of frozen fringe as a function of melt and pressure."""
-        pass
+    def calc_thermal_gradients(self):
+        """Calculates the temperature gradients through the frozen fringe and dispersed layer."""
+        required = ['glacier__sliding_velocity', 'glacier__effective_pressure']
+
+        for field in required:
+            if field not in self.grid.at_node.keys():
+                raise ValueError("Missing " + str(field) + " at nodes.")
+
+        if 'fringe__thermal_gradient' not in self.grid.at_node.keys():
+            self.grid.add_zeros('fringe__thermal_gradient', at = 'node')
+
+        if 'frozen_fringe__thickness' not in self.grid.at_node.keys():
+            self.grid.add_zeros('frozen_fringe__thickness', at = 'node')
+
+        # Calculate entry pressure
+        if 'entry_pressure' not in self.parameters.keys():
+            gamma = self.parameters['surface_energy']
+            rp = self.parameters['pore_throat_radius']
+            self.parameters['entry_pressure'] = (2 * gamma) / rp
+
+        # Calculate temperature at base of fringe
+        if 'fringe_base_temperature' not in self.parameters.keys():
+            pf = self.parameters['entry_pressure']
+            Tm = self.parameters['melt_temperature']
+            rho = self.parameters['ice_density']
+            L = self.parameters['ice_latent_heat']
+            self.parameters['fringe_base_temperature'] = Tm - ((pf * Tm) / (rho * L))
+
+        if 'fringe_conductivity' not in self.parameters.keys():
+            ki = self.parameters['ice_thermal_conductivity']
+            ks = self.parameters['sediment_thermal_conductivity']
+            phi = self.parameters['frozen_fringe_porosity']
+            self.parameters['fringe_conductivity'] = (1 - phi) * ks + phi * ki
+
+        if 'frictional_heat__flux' not in self.grid.at_node.keys():
+            self.calc_melt_rate()
+
+        K = self.parameters['fringe_conductivity']
+        Qg = self.parameters['geothermal_heat_flux']
+        Qf = self.grid.at_node['frictional_heat__flux'][:]
+        self.grid.at_node['fringe__thermal_gradient'][:] = -(Qg + Qf) / K
+
+        G = self.grid.at_node['fringe__thermal_gradient'][:]
+        hf = self.grid.at_node['frozen_fringe__thickness'][:]
+        Tf = self.parameters['fringe_base_temperature']
+
+        self.grid.at_node['transition_temperature'] = G * hf + Tf
 
     def calc_regelation_rate(self):
-        """Calculates the vertical regelation rate as a function of heat fluxes and pressure."""
-        pass
+        """Calculates the vertical regelation rate and change in depth-averaged sediment concentration."""
+        required = []
+
+        for field in required:
+            if field not in self.grid.at_node.keys():
+                raise ValueError("Missing " + str(field) + " at nodes.")
+
+        if 'particle__vertical_velocity' not in self.grid.at_node.keys():
+            self.grid.add_zeros('particle__vertical_velocity', at = 'node')
+
+        if 'cluster__vertical_velocity' not in self.grid.at_node.keys():
+            self.grid.add_zeros('cluster__vertical_velocity', at = 'node')
+
+    def calc_fringe_growth_rate(self):
+        """Calculates the growth rate of frozen fringe as a function of melt and pressure."""
+        required = ['glacier__effective_pressure']
+
+        for field in required:
+            if field not in self.grid.at_node.keys():
+                raise ValueError("Missing " + str(field) + " at nodes.")
+
+        if 'fringe__undercooling' not in self.grid.at_node.keys():
+            self.grid.add_zeros('fringe__undercooling', at = 'node')
+
+        if 'fringe__saturation' not in self.grid.at_node.keys():
+            self.grid.add_zeros('fringe__saturation', at = 'node')
+
+        if 'nominal__heave_rate' not in self.grid.at_node.keys():
+            self.grid.add_zeros('nominal__heave_rate', at = 'node')
+
+        if 'flow__resistance' not in self.grid.at_node.keys():
+            self.grid.add_zeros('flow__resistance', at = 'node')
+
+        self.calc_thermal_gradients()
+
+        G = self.grid.at_node['fringe__thermal_gradient'][:]
+        h = self.grid.at_node['frozen_fringe__thickness'][:]
+        Tm = self.parameters['melt_temperature']
+        Tf = self.parameters['fringe_base_temperature']
+        self.grid.at_node['fringe__undercooling'][:] = 1 - ((G * h) / (Tm - Tf))
+
+        alpha = self.parameters['fringe_alpha']
+        beta = self.parameters['fringe_beta']
+        theta = self.grid.at_node['fringe__undercooling'][:]
+        self.grid.at_node['fringe__saturation'][:] = 1 - theta**(-beta)
+
+        rho_w = self.parameters['water_density']
+        rho_i = self.parameters['ice_density']
+        L = self.parameters['ice_latent_heat']
+        k0 = self.parameters['permeability']
+        eta = self.parameters['water_viscosity']
+        self.grid.at_node['nominal__heave_rate'][:] = -(rho_w**2 * L * G * k0) / (rho_i * Tm * eta)
+
+        d = self.parameters['film_thickness']
+        R = self.parameters['till_grain_radius']
+        self.grid.at_node['flow__resistance'][:] = -(rho_w**2 * k0 * G * R**2) / (rho_i**2 * (Tm - Tf) * d**3)
+
+        phi = self.parameters['frozen_fringe_porosity']
+        Vs = self.grid.at_node['nominal__heave_rate'][:]
+        Pi = self.grid.at_node['flow__resistance'][:]
+        N = self.grid.at_node['glacier__effective_pressure'][:]
+        pf = self.parameters['entry_pressure']
+
+        # Throwaway variables for long coefficients
+        A = theta + phi * (1 - theta + (1 / (1 - beta)) * (theta**(1 - beta) - 1))
+        B = ((1 - phi)**2 / (alpha + 1)) * (theta**(alpha + 1) - 1)
+        C = ((2 * (1 - phi) * phi) / (alpha - beta + 1)) * (theta**(alpha - beta + 1) - 1)
+        D = (phi**2 / (alpha - 2 * beta + 1)) * (theta**(alpha - 2 * beta + 1) - 1)
+
+        self.grid.at_node['fringe__heave_rate'][:] = Vs * (A - (N / pf)) / (B + C + D + Pi)
+        V = self.grid.at_node['fringe__heave_rate'][:]
+        m = self.grid.at_node['subglacial_melt__rate'][:]
+        S = self.grid.at_node['fringe__saturation'][:]
+
+        self.grid.at_node['fringe__growth_rate'] = (-m - V) / (phi * S)
 
     def calc_advective_deformation(self):
-        """Calculates thickening or thinning of ice as a function of the velocity field."""
-        pass
+        """Calculates thickening or thinning of basal ice as a function of the velocity field."""
+        required = ['glacier__sliding_velocity']
+
+        for field in required:
+            if field not in self.grid.at_node.keys():
+                raise ValueError("Missing " + str(field) + " at nodes.")
+
+        if 'advective__deformation' not in self.grid.at_node.keys():
+            self.grid.add_zeros('advective__deformation', at = 'node')
+
+        H = self.grid.at_node['glacier__thickness'][:]
+        ub = self.grid.at_node['glacier__sliding_velocity'][:]
+        grad_u = self.grid.calc_grad_at_link(ub)
+        div_u = self.grid.calc_flux_div_at_node(grad_u)
 
     def run_one_step(self, dt):
         """Advances the model forward one time step."""
--- basis/test/test_basal_ice_stratigrapher.py
+++ basis/test/test_basal_ice_stratigrapher.py
@@ -26,17 +26,82 @@ def test_calc_erosion_rate():
     basis = BasalIceStratigrapher(config)
     basis.calc_erosion_rate()
 
-    assert_array_equal(basis.grid.at_node['erosion__rate'], [4e-2, 4e-2, 4e-2, 4e-2,
-                                                             4e-2, 4e-2, 4e-2, 4e-2,
-                                                             4e-2, 4e-2, 4e-2, 4e-2,
-                                                             4e-2, 4e-2, 4e-2, 4e-2])
+    assert_array_equal(basis.grid.at_node['erosion__rate'], [1.256e-9, 1.256e-9, 1.256e-9, 1.256e-9,
+                                                             1.256e-9, 1.256e-9, 1.256e-9, 1.256e-9,
+                                                             1.256e-9, 1.256e-9, 1.256e-9, 1.256e-9,
+                                                             1.256e-9, 1.256e-9, 1.256e-9, 1.256e-9])
 
 def test_calc_melt_rate():
     """Test the melt rate calculation."""
     basis = BasalIceStratigrapher(config)
     basis.calc_melt_rate()
 
-    assert_array_almost_equal(basis.grid.at_node['subglacial_melt__rate'], [0.01959, 0.01959, 0.01959, 0.01959,
-                                                                            0.01959, 0.01959, 0.01959, 0.01959,
-                                                                            0.01959, 0.01959, 0.01959, 0.01959,
-                                                                            0.01959, 0.01959, 0.01959, 0.01959])
+    assert_array_almost_equal(basis.grid.at_node['frictional_heat__flux'], [0.188, 0.188, 0.188, 0.188,
+                                                                            0.188, 0.188, 0.188, 0.188,
+                                                                            0.188, 0.188, 0.188, 0.188,
+                                                                            0.188, 0.188, 0.188, 0.188], 3)
+
+    assert_array_almost_equal(basis.grid.at_node['subglacial_melt__rate'], [8.11e-10, 8.11e-10, 8.11e-10, 8.11e-10,
+                                                                            8.11e-10, 8.11e-10, 8.11e-10, 8.11e-10,
+                                                                            8.11e-10, 8.11e-10, 8.11e-10, 8.11e-10,
+                                                                            8.11e-10, 8.11e-10, 8.11e-10, 8.11e-10])
+
+def test_calc_thermal_gradients():
+    """Test thermal gradient calculations."""
+    basis = BasalIceStratigrapher(config)
+    basis.calc_thermal_gradients()
+
+    assert_array_almost_equal(basis.parameters['entry_pressure'], 68e3)
+    assert_array_almost_equal(basis.parameters['fringe_base_temperature'], 272.9394, 3)
+    assert_array_almost_equal(basis.parameters['fringe_conductivity'], 4.56, 2)
+    assert_array_almost_equal(basis.grid.at_node['fringe__thermal_gradient'], [-0.05, -0.05, -0.05, -0.05,
+                                                                               -0.05, -0.05, -0.05, -0.05,
+                                                                               -0.05, -0.05, -0.05, -0.05,
+                                                                               -0.05, -0.05, -0.05, -0.05], 2)
+
+    assert_array_almost_equal(basis.grid.at_node['transition_temperature'], [272.9394, 272.9394, 272.9394, 272.9394,
+                                                                             272.9394, 272.9394, 272.9394, 272.9394,
+                                                                             272.9394, 272.9394, 272.9394, 272.9394,
+                                                                             272.9394, 272.9394, 272.9394, 272.9394], 3)
+
+def test_calc_fringe_growth():
+    """Test frozen fringe calculations."""
+    basis = BasalIceStratigrapher(config)
+    basis.calc_fringe_growth_rate()
+
+    assert_array_almost_equal(basis.grid.at_node['fringe__undercooling'], [1.0008, 1.0008, 1.0008, 1.0008,
+                                                                           1.0008, 1.0008, 1.0008, 1.0008,
+                                                                           1.0008, 1.0008, 1.0008, 1.0008,
+                                                                           1.0008, 1.0008, 1.0008, 1.0008], 4)
+
+    assert_array_almost_equal(basis.grid.at_node['fringe__saturation'], [0.001, 0.001, 0.001, 0.001,
+                                                                         0.001, 0.001, 0.001, 0.001,
+                                                                         0.001, 0.001, 0.001, 0.001,
+                                                                         0.001, 0.001, 0.001, 0.001], 3)
+
+    assert_array_almost_equal(basis.grid.at_node['nominal__heave_rate'], [1.655e-9, 1.655e-9, 1.655e-9, 1.655e-9,
+                                                                          1.655e-9, 1.655e-9, 1.655e-9, 1.655e-9,
+                                                                          1.655e-9, 1.655e-9, 1.655e-9, 1.655e-9,
+                                                                          1.655e-9, 1.655e-9, 1.655e-9, 1.655e-9])
+
+    assert_array_almost_equal(basis.grid.at_node['flow__resistance'], [0.07, 0.07, 0.07, 0.07,
+                                                                       0.07, 0.07, 0.07, 0.07,
+                                                                       0.07, 0.07, 0.07, 0.07,
+                                                                       0.07, 0.07, 0.07, 0.07], 3)
+
+    assert_array_almost_equal(basis.grid.at_node['fringe__heave_rate'], [-1.095e-8, -1.095e-8, -1.095e-8, -1.095e-8,
+                                                                         -1.095e-8, -1.095e-8, -1.095e-8, -1.095e-8,
+                                                                         -1.095e-8, -1.095e-8, -1.095e-8, -1.095e-8,
+                                                                         -1.095e-8, -1.095e-8, -1.095e-8, -1.095e-8])
+
+    assert_array_almost_equal(basis.grid.at_node['fringe__growth_rate'], [2.173e-5, 2.173e-5, 2.173e-5, 2.173e-5,
+                                                                          2.173e-5, 2.173e-5, 2.173e-5, 2.173e-5,
+                                                                          2.173e-5, 2.173e-5, 2.173e-5, 2.173e-5,
+                                                                          2.173e-5, 2.173e-5, 2.173e-5, 2.173e-5])
+
+def test_advective_deformation():
+    """Test advective deformation calculation."""
+    basis = BasalIceStratigrapher(config)
+    basis.calc_advective_deformation()
+
+    pass
